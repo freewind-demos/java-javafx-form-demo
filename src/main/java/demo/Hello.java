f29cd32b9c535c100fdfcbc8d22366b3c49c495c
freewind <nowindlee@gmail.com>
2018-07-14 18:00:14 +0800

    use grid pane to make it look better

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -3,11 +3,12 @@ package demo;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
@@ -18,7 +19,7 @@ public class Hello extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello");
-        StackPane root = new StackPane() {{
+        Pane root = new Pane() {{
             getChildren().add(createForm());
         }};
         primaryStage.setScene(new Scene(root, 300, 250));
@@ -26,20 +27,22 @@ public class Hello extends Application {
     }
 
     private Node createForm() {
-        return new VBox() {{
-            getChildren().addAll(
-                    new Label("Username"),
-                    new TextField("myname"),
-                    new Label("Password"),
-                    new PasswordField(),
-                    new Button("Login") {{
-                        setOnAction(new EventHandler<ActionEvent>() {
-                            public void handle(ActionEvent event) {
-                                new Alert(Alert.AlertType.INFORMATION, "Login now").showAndWait();
-                            }
-                        });
-                    }}
-            );
+        return new GridPane() {{
+            setHgap(10);
+            setVgap(10);
+            setPadding(new Insets(0, 10, 0, 10));
+
+            add(new Label("Username"), 0, 0);
+            add(new TextField("myname"), 1, 0);
+            add(new Label("Password"), 0, 1);
+            add(new PasswordField(), 1, 1);
+            add(new Button("Login") {{
+                setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent event) {
+                        new Alert(Alert.AlertType.INFORMATION, "Login now").showAndWait();
+                    }
+                });
+            }}, 1, 2);
         }};
     }
 }
